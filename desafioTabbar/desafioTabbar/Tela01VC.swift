850f417121b4d24a7cc698a733fcfb9d3e285f30
Alysson Menezes <almenezes912@gmail.com>
2022-11-22 18:01:47 -0300

    configurando tabbar e adicionando classes ao projeto

--- desafioTabbar/desafioTabbar/Tela01VC.swift
+++ desafioTabbar/desafioTabbar/Tela01VC.swift
@@ -7,8 +7,12 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class Tela01VC: UIViewController {
 
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
---
+++ desafioTabbar/desafioTabbar/Tela02VC.swift
@@ -0,0 +1,18 @@
+//
+//  Tela02VC.swift
+//  desafioTabbar
+//
+//  Created by ALYSSON DODO on 22/11/22.
+//
+
+import UIKit
+
+class Tela02VC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
